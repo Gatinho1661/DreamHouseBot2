1b716e2b3a0404cbbeb137d4f2415f0e35c06b98
Gatinho1661 <play9788@gmail.com>
2021-06-25 20:37:05 -0300

    pequena mudaça no log

--- eventos/executado.js
+++ eventos/executado.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const { MessageEmbed } = require("discord.js");
 
-// Emitido quando um comando é executado
+// Emitido quando um comando termina de ser executado
 module.exports = async (client, excTempo, cmd, msg, args) => {
-    client.log("comando", `${client.commandPrefix}${cmd.name} foi executado em ${(new Date().getTime() - excTempo.getTime())}ms`);
+    client.log("comando", `${client.commandPrefix}${cmd.name} terminou de ser executado depois de ${(new Date().getTime() - excTempo.getTime())}ms`);
 }
