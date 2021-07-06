ed4f1e19ae8d510db3192290407b04cde202e3c9
Gatinho1661 <play9788@gmail.com>
2021-07-06 00:14:30 -0300

    Retirar discord.js-commando

--- bot.js
+++ bot.js
@@ -1,11 +1,9 @@
-const Commando = require('discord.js-commando');
-const path = require("path");
+const Discord = require('discord.js');
+//const path = require("path");
 const Enmap = require("enmap");
 require('dotenv').config()
 
-const eventos = require("./modulos/eventos");
-
-global.client = new Commando.Client({ // define client como um objeto global
+global.client = new Discord.Client({ // define client como um objeto global
     intents: [
         'GUILDS',
         'GUILD_MEMBERS',
@@ -24,17 +22,24 @@ global.client = new Commando.Client({ // define client como um objeto global
         //'DIRECT_MESSAGE_TYPING',
     ],
     partials: ['MESSAGE', 'CHANNEL', 'REACTION', 'GUILD_MEMBER', 'USER'],
-    owner: process.env.dono.split(" "),   // Donos do bot
-    commandPrefix: process.env.prefixo,   // Prefixo geral do bot
-    nonCommandEditable: true,             // Se as mensagens sem comandos podem ser editadas para um comando
-    commandEditableDuration: 10           // Tempo em segundos que as mensagens de comando devem ser editáveis
+    presence: {
+        activities: [
+            {
+                name: "testes",
+                type: "WATCHING"
+            }
+        ]
+    }
 });
 
 client.defs = require("./data/defs.json");
 client.log = require("./modulos/log.js");
 client.responder = require("./modulos/responder.js");
 client.dir = __dirname
+client.prefixo = process.env.prefixo
+client.dono = process.env.dono.split(" ")
 
+client.comandos = new Discord.Collection()
 client.config = new Enmap("config");
 client.usuarioOld = new Enmap("usuario"); // Para compatibilidade com o Banco de dados antigo
 client.usuarios = new Enmap("usuarios");
@@ -45,28 +50,8 @@ process.on("uncaughtException", (erro) => {
     client.log("critico", `Unhandled error: ${erro.stack}`);
 });
 
-client.registry
-    // Registrar grupos de comandos
-    .registerGroups([
-        ['ações', 'Gifs de ações'],
-        ['administração', 'Umas coisinhas para não deixar adms a desejar'],
-        ['audios', 'Alguns audios para tocar em call'],
-        ['dono', 'Algumas coisinhas divertidas'],
-        ['perfil', 'Definições do seu perfil'],
-        ['relacionamento', 'Amor está no ar, e o ódio também'],
-        ['utilitários', 'Comandos úteis para facilitar sua vida']
-    ])
-
-    // Registrar comandos padrões
-    //.registerDefaultCommands()
-
-    // Registra tipos de argumentos padrão
-    .registerDefaultTypes()
-
-    // Registrar todos os comandos da pasta comandos
-    .registerCommandsIn(path.join(client.dir, 'comandos'));
-
-eventos(client); // Registrar eventos
+require("./modulos/comandos")()
+require("./modulos/eventos")(); // Registrar eventos
 
 // Fazer login
 client.login(process.env.TOKEN);
