c5c1af74728777b2276399f3a8be0d3f5be83020
Gatinho1661 <play9788@gmail.com>
2021-07-09 21:48:40 -0300

    Comando de aniversario

--- comandos/perfil/aniversario.js
+++ comandos/perfil/aniversario.js
@@ -1,4 +1,5 @@
 const { MessageButton, MessageEmbed } = require("discord.js");
+const { formatarCanal } = require("../../modulos/utils")
 const chrono = require('chrono-node');
 //const cron = require('node-cron');
 
@@ -27,7 +28,7 @@ module.exports = {
         if (!args[0]) return client.responder(msg, this, "uso", "⛔ Faltando argumentos", "Você tem que enviar sua data de nascimento ou de aniversário");
 
         //* define os dados do usuario da pessoa caso nao tenha
-        client.usuario.ensure(`${msg.author.id}`, {
+        client.usuarios.ensure(`${msg.author.id}`, {
             textinho: null,
             aniversario: null,
             idade: null,
@@ -38,105 +39,170 @@ module.exports = {
         });
 
         //* Pegar dados do usuário
-        const usuario = client.usuario.get(msg.author.id);
+        const usuario = client.usuarios.get(msg.author.id);
+        const aniversario = new Date(usuario.aniversario)
 
-        if (usuario.aniversario === null) {
-            //* Adicionar aniversario
+        //* Transformar texto em data
+        let data = chrono.pt.strict.parseDate(args[0])
+        if (!data) return client.responder(msg, this, "uso", "⛔ Argumentos errados", "Você tem que enviar sua data de nascimento");
+        data.setHours(0, 0, 0);
 
-            //* Transformar texto em data
-            let data = chrono.pt.strict.parseDate(args[1])
-            if (!data) return client.responder(msg, this, "uso", "⛔ Argumentos errados", "Você tem que enviar sua data de nascimento ou de aniversário");
+        //* Calcular idade
+        const idade = new Date().getFullYear() - data.getFullYear();
+        if (idade < 1) return client.responder(msg, this, "uso", "⛔ Argumentos errados", `Você tem que enviar sua data de nascimento, se você não quer dizer sua idade fale com <!@${client.dono[0]}>`);
 
-            if (data.getFullYear() !== new Date().getFullYear()) {
-                //* Se tiver ano definido
-                const idade = new Date().getFullYear() - data.getFullYear();
-                //TODO verificar se nasceu no futuro ou tem menos de 10 anos
+        if (aniversario.getTime() === data.getTime()) return client.responder(msg, this, "uso", "⛔ Argumentos errados", `Sua data de nascimento já está definido para esse dia`);
 
-                //* Cria os botões e a embed
-                const sim = new MessageButton()
-                    .setCustomID(`sim`)
-                    .setLabel('Sim')
-                    //.setEmoji("✅")
-                    .setDisabled(false)
-                    .setStyle("SUCCESS");
-
-                const nao = new MessageButton()
-                    .setCustomID('nao')
-                    .setLabel('Não')
-                    //.setEmoji("❌")
-                    .setDisabled(false)
-                    .setStyle("DANGER");
-
-                const Embed = new MessageEmbed()
-                    .setColor(client.defs.corEmbed.carregando)
-                    .setTitle('❓ Tem certeza?')
-                    .addFields([
-                        { name: "Aniversário", value: `${data.getDate()}/${data.getMonth() + 1}`, inline: true },
-                        { name: "Idade", value: `${idade} anos`, inline: true }
-                    ])
-                    .setFooter("escolha clicando nos botões");
-                const resposta = await msg.channel.send({
-                    content: null,
-                    embeds: [Embed],
-                    components: [[sim, nao]],
-                    reply: { messageReference: msg }
-                }).catch();
-
-
-                //* Inicia coletor de botões
-                const filtro = (interaction) => interaction.user.id === msg.author.id;
-                resposta.awaitMessageComponent({ filtro, time: 60000 })
-                    .then(async i => {
-                        switch (i.customID) {
-
-                            case "sim": {
-                                client.usuarios.set(msg.author.id, `${data.getFullYear()} ${data.getMonth() + 1} ${data.getDate()} 00:00:00`, 'aniversario');
-                                break;
-                            }
-                            case "nao": {
-                                // TODO nao
-                                break;
-                            }
-                            default: {
-                                client.log("erro", `Um botão chamado "${i.customID}" foi precionado, mais nenhuma ação foi definida`)
-                                break;
-                            }
-                        }
-                        client.log("verbose", `@${i.user.tag} apertou "${i.customID}" id:${msg.id}`)
-                    }).catch(err => {
-
-                        client.log("erro", err.stack)
-                        client.log("comando", `Ocorreu um erro em ${this.name} ao ser executado por @${msg.author.tag}`, "erro");
-
-                        const erro = new MessageButton()
-                            .setCustomID(`erro`)
-                            .setLabel('Ocorreu um erro')
-                            .setDisabled(true)
-                            .setStyle('DANGER');
-
-                        resposta.edit({
-                            content: resposta.content || null,
-                            embeds: resposta.embeds,
-                            components: [[erro]]
-                        }).catch();
-                    })
-            }
-        }
+        const sim = new MessageButton()
+            .setCustomId(`sim`)
+            .setLabel(`Sim`)
+            .setDisabled(false)
+            .setStyle(`SUCCESS`);
 
         const editar = new MessageButton()
-            .setCustomID("editar")
-            .setLabel(usuario.aniversario ? "editar" : "adicionar")
+            .setCustomId("editar")
+            .setLabel("Editar")
+            .setDisabled(false)
             .setStyle("PRIMARY");
 
+        const cancelar = new MessageButton()
+            .setCustomId('cancelar')
+            .setLabel('Cancelar')
+            .setDisabled(false)
+            .setStyle("DANGER");
+
+        const adicionando = usuario.aniversario === null
+        let botoes = adicionando ? [[sim, cancelar]] : [[editar, cancelar]];
 
         const Embed = new MessageEmbed()
             .setColor(client.defs.corEmbed.carregando)
-            .setTitle('🎉 Aniversário')
-            .addFields([
-                { name: "Nascimento", value: usuario.aniversario ? new Date(usuario.aniversario).toLocaleDateString() : "não definido", inline: true },
-                { name: "Idade", value: usuario.idade ? `${usuario.idade} anos` : `??`, inline: true }
-            ])
+            .setTitle(adicionando ? '❓ Adicionar aniversário' : '❓ Editar aniversário')
             .setFooter("escolha clicando nos botões");
-        const resposta = await msg.channel.send({ content: null, embeds: [Embed], components: [[editar]] }).catch();
+        adicionando
+            ? Embed.addFields([
+                { name: "Você nasceu em", value: `<t:${data.getTime() / 1000}:d> <t:${data.getTime() / 1000}:R>`, inline: false },
+            ])
+            : Embed.addFields([
+                { name: "Você nasceu em", value: `<t:${aniversario.getTime() / 1000}:d> <t:${aniversario.getTime() / 1000}:R>`, inline: false },
+                { name: "Você deseja editar para", value: `<t:${data.getTime() / 1000}:d> <t:${data.getTime() / 1000}:R>`, inline: false },
+            ])
+        const resposta = await msg.channel.send({
+            content: null,
+            embeds: [Embed],
+            components: botoes,
+            reply: { messageReference: msg }
+        }).catch();
+
+        const respostas = {
+            sim(i) {
+                client.usuarios.set(msg.author.id, `${data.getFullYear()} ${data.getMonth() + 1} ${data.getDate()} 00:00:00`, 'aniversario');
+                client.usuarios.set(msg.author.id, idade, 'idade');
+                client.log("info", `Aniversário de ${msg.author.tag} foi definido para ${data.toLocaleDateString()} e com ${idade} anos`);
+
+                Embed
+                    .setColor(client.defs.corEmbed.sim)
+                    .setTitle("✅ Adicionar aniversário")
+                    .setFooter("");
+                botoes = [[sim.setDisabled(true)]];
+
+                i.update({
+                    content: resposta.content || null,
+                    embeds: [Embed],
+                    components: botoes
+                });
+            },
+            editar(i) {
+                client.usuarios.set(msg.author.id, `${data.getFullYear()} ${data.getMonth() + 1} ${data.getDate()} 00:00:00`, 'aniversario');
+                client.usuarios.set(msg.author.id, idade, 'idade');
+                client.log("info", `Aniversário de ${msg.author.tag} foi definido para ${data.toLocaleDateString()} e com ${idade} anos`);
+
+                Embed
+                    .setColor(client.defs.corEmbed.sim)
+                    .setTitle("✅ Editar aniversário")
+                    .setFooter("");
+                botoes = [[editar.setDisabled(true)]];
+
+                i.update({
+                    content: resposta.content || null,
+                    embeds: [Embed],
+                    components: botoes
+                });
+            },
+            cancelar(i) {
+                client.log("info", `Cancelado`);
+
+                Embed
+                    .setColor(client.defs.corEmbed.nao)
+                    .setTitle("❌ Cancelado")
+                    .setFooter("");
+                botoes = [[cancelar.setDisabled(true)]];
+
+                i.update({
+                    content: resposta.content || null,
+                    embeds: [Embed],
+                    components: botoes
+                });
+            }
+        }
+
+        const coletor = resposta.createMessageComponentCollector({ time: 60000 })
+        client.log("info", `Coletor de botões iniciado em #${formatarCanal(msg.channel)} por @${msg.author.tag} id:${msg.id}`);
+
+        coletor.on("collect", i => {
+            if (i.user.id !== msg.author.id) {
+                client.log("verbose", `@${i.user.tag} apertou "${i.customId}", mas foi bloqueado id:${msg.id}`);
+
+                const cuidaEmbed = new MessageEmbed()
+                    .setColor(client.defs.corEmbed.nao)
+                    .setTitle(`⛔ Cuida da sua vida`)
+                    .setDescription("essa mensagem não foi direcionada a você");
+                return i.reply({ content: null, embeds: [cuidaEmbed], ephemeral: true })
+            }
+
+            try {
+                client.log("verbose", `@${i.user.tag} apertou "${i.customId}" id:${msg.id}`)
+                respostas[i.customId](i);
+            } catch (err) {
+                client.log("erro", err.stack)
+                client.log("comando", `Ocorreu um erro em ${this.nome} ao ser executado por @${msg.author.tag}`, "erro");
+
+                coletor.stop("erro")
+            } finally {
+                coletor.stop("respondido");
+            }
+        })
+
+        coletor.once('end', (coletado, razao) => {
+            client.log("info", `Coletor de botões terminado por ${razao} em #${formatarCanal(msg.channel)}, coletando ${coletado.size} interações id:${msg.id}`);
+
+            if (razao === "erro") {
+                const erro = new MessageButton()
+                    .setCustomId(`erro`)
+                    .setLabel('Ocorreu um erro')
+                    .setDisabled(true)
+                    .setStyle('DANGER');
+                botoes = [[erro]]
+
+                resposta.edit({
+                    content: resposta.content || null,
+                    embeds: resposta.embeds,
+                    components: botoes
+                }).catch();
+            }
+            if (razao === "time") {
+                const tempo = new MessageButton()
+                    .setCustomId(`tempo`)
+                    .setLabel("Tempo esgotado")
+                    .setDisabled(true)
+                    .setStyle('SECONDARY');
+                botoes = [[tempo]];
+
+                resposta.edit({
+                    content: resposta.content || null,
+                    embeds: resposta.embeds,
+                    components: botoes
+                }).catch();
+            }
+        });
     }
 };
