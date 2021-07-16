b315415fcece127262eebc268f64ffd2bf0bf451
Gatinho1661 <play9788@gmail.com>
2021-07-16 13:12:17 -0300

    categorias

--- modulos/comandos.js
+++ modulos/comandos.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 module.exports = () => {
     for (const pasta of fs.readdirSync(client.dir + `/comandos/`)) {
-        //? adicionar grupos
+        //? adicionar categoria
 
         for (const arquivo of fs.readdirSync(client.dir + `/comandos/${pasta}`)) {
             if (!arquivo.endsWith(".js")) continue
@@ -11,23 +11,35 @@ module.exports = () => {
             try {
                 const comando = require(client.dir + `/comandos/${pasta}/${arquivo}`);
 
+                //* Verificar se já existe um comando com esse nome
+                if (client.comandos.some(cmd => cmd.sinonimos.includes(comando.nome))) {
+                    throw new Error(`O nome "${comando.nome}" já está registrado como um sinônimo de um comando`);
+                }
                 if (client.comandos.some(cmd => cmd.nome === comando.nome)) {
                     throw new Error(`Um comando com o nome "${comando.nome}" já está registrado`);
                 }
-                for (const sinonimos of comando.sinonimos) {
-                    if (client.comandos.some(cmd => cmd.nome === sinonimos || cmd.sinonimos.includes(sinonimos))) {
-                        throw new Error(`Um comando com o sinonimo "${sinonimos}" já está registrado`)
+
+                //* Verificar se já existe algum sinônimo
+                for (const sinonimo of comando.sinonimos) {
+                    if (client.comandos.some(cmd => cmd.sinonimos.includes(sinonimo))) {
+                        throw new Error(`Um comando com o sinônimo "${sinonimo}" já está registrado`);
+                    }
+                    if (client.comandos.some(cmd => cmd.nome === sinonimo)) {
+                        throw new Error(`O sinônimo "${sinonimo}" já está registrado como o nome de um comando`);
                     }
                 }
 
-                comando.grupo = pasta;
+                //* Definir a categoria do comando
+                if (client.defs.categorias[pasta]) comando.categoria = pasta;
+                else throw new Error(`Comando sem categoria`);
+
                 client.comandos.set(comando.nome, comando);
-                //client.log("bot", `Comando "${comando.nome}" carregado`);
+                client.log("verbose", `${comando.nome} foi registrado`);
             } catch (err) {
                 client.log("critico", `${err.message}, o comando "${pasta}/${arquivo}" será ignorado`);
             }
         }
     }
     client.log("bot", `Comandos:`, null, true);
-    console.table(client.comandos.array(), ["grupo", "nome", "sinonimos"]);
+    console.table(client.comandos.array(), ["categoria", "nome", "sinonimos"]);
 }
