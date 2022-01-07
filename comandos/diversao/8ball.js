5fbef20311ef1fb8084956209d563c70f9efbb0d
Gatinho1661 <play9788@gmail.com>
2022-01-07 02:17:04 -0300

    comandos de diversão

---
+++ comandos/diversao/8ball.js
@@ -0,0 +1,52 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    //* Infomações do comando
+    emoji: "🎱",
+    nome: "8ball",
+    sinonimos: [],
+    descricao: "Faça uma pergunta e tenha uma resposta definitiva para seus problemas",
+    exemplos: [
+        { comando: "8ball [pergunta]", texto: "Pergunta alguma coisa" },
+    ],
+    args: "",
+    opcoes: [
+        {
+            name: "pergunta",
+            description: "Sua pergunta para eu responder para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: true
+        },
+    ],
+    canalVoz: false,
+    contaPrimaria: false,
+    apenasServidor: false,
+    apenasDono: false,
+    nsfw: false,
+    permissoes: {
+        usuario: [],
+        bot: ["SEND_MESSAGES"]
+    },
+    cooldown: 1,
+    escondido: false,
+    suporteBarra: true,
+    testando: true,
+
+    //* Comando
+    async executar(iCmd, opcoes) {
+        const respostas = ["Sim", "Claro que sim", "Provavelmente sim", "Provavelmente não", "Claro que não", "Não"]
+        const aleatorio = Math.floor(Math.random() * respostas.length);
+
+        const pergunta = opcoes.pergunta.slice(0, 2045);
+
+        const EmbedPergunta = new MessageEmbed()
+            .setColor(iCmd.member.displayColor ? iCmd.member.displayHexColor : client.defs.corEmbed.normal)
+            .setAuthor({ name: iCmd.member.displayName, iconURL: iCmd.member.displayAvatarURL({ dynamic: true, size: 32 }) })
+            .setDescription(`${pergunta}`)
+        const EmbedResposta = new MessageEmbed()
+            .setColor(iCmd.guild.me.displayColor ? iCmd.guild.me.displayHexColor : client.defs.corEmbed.normal)
+            .setAuthor({ name: iCmd.guild.me.displayName, iconURL: iCmd.guild.me.displayAvatarURL({ dynamic: true, size: 32 }) })
+            .setDescription(`${respostas[aleatorio]}`)
+        iCmd.reply({ content: null, embeds: [EmbedPergunta, EmbedResposta] }).catch();
+    }
+}
---
+++ comandos/diversao/escolher.js
@@ -0,0 +1,191 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    //* Infomações do comando
+    emoji: "🤔",
+    nome: "escolher",
+    sinonimos: [],
+    descricao: "Precisa de ajuda para escolher alguma coisa? Deixa comigo",
+    exemplos: [
+        { comando: "escolher [escolha1] [escolha1] ...", texto: "Escolha entre 2 ou + escolhas" },
+    ],
+    args: "",
+    opcoes: [
+        {
+            name: "escolha1",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: true
+        },
+        {
+            name: "escolha2",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: true
+        },
+        {
+            name: "escolha3",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha4",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha5",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha6",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha7",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha8",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha9",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha10",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha11",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha12",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha13",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha14",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha15",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha16",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha17",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha18",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha19",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha20",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha21",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha22",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha23",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha24",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        },
+        {
+            name: "escolha25",
+            description: "Lista de opções para escolher para você",
+            type: client.defs.tiposOpcoes.STRING,
+            required: false
+        }
+    ],
+    canalVoz: false,
+    contaPrimaria: false,
+    apenasServidor: false,
+    apenasDono: false,
+    nsfw: false,
+    permissoes: {
+        usuario: [],
+        bot: ["SEND_MESSAGES"]
+    },
+    cooldown: 1,
+    escondido: false,
+    suporteBarra: true,
+    testando: true,
+
+    //* Comando
+    async executar(iCmd, opcoes) {
+        //* Pegar todas as opcoes
+        const escolhas = Object.values(opcoes).filter(e => e);
+        const escolha = escolhas[Math.floor(Math.random() * escolhas.length)];
+
+        const Embed = new MessageEmbed()
+            .setColor(client.defs.corEmbed.normal)
+            .setTitle("🤔 Eu escolho")
+            .setDescription(`${escolha}`)
+        iCmd.reply({ content: null, embeds: [Embed] }).catch();
+    }
+}
