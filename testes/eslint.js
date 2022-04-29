bbaf9aa6a05c53130755fae2fcb1b12246a16cb9
Gatinho1661 <play9788@gmail.com>
2022-04-29 14:55:19 -0300

    arquivo de teste eslint

---
+++ testes/eslint.js
@@ -0,0 +1,16 @@
+var varNaoUsada;;var c;//uma constante
+
+if (!!null) {
+
+} else {
+
+};
+
+//TODO Cometer um crime
+
+function /* meteu essa */ teste(){
+  for (var i = 0; i < 10; i--) {
+    return
+    console.log("Mano quantos crimes");
+  }
+}
